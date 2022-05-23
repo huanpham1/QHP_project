7eece225d76c0b8e4821aea3f02ed827a7e65a32
huanpham <huanpham1508@gmail.com>
2022-05-23 22:25:59 +0700

    add SanPham

---
+++ QHP_project/app/Models/SanPham.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+class SanPham extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'TenTaiKhoan','matkhau',
+    ];
+
+    public function GetSanPham($id){
+        $data = DB::table('sanpham')->where('MaSP', $id)->first();
+        // dd($data);
+        return $data;
+    }
+    public function GetSoLuong($id){
+        $data = (DB::table('chitietsanpham')->where('MaSP', $id)->first());
+
+        return $data;
+    }
+    public function getSize($id,){
+        $data = DB::table('chitietsanpham')->where('MaSP', $id)->get('Size');
+        return $data;
+    }
+    public function getSizenew($id, $size){
+        // DB::table('chitietsanpham')->where('MaSP', '1')->where('size','3')->get('SoLuongCon')[0]->SoLuongCon
+        $data = (DB::table('chitietsanpham')->where('MaSP', (string)$id)->where('size',(string)$size)->get('SoLuongCon')[0]->SoLuongCon);
+        return $data;
+    }
+    public function getCTSPID($id, $size){
+        $data = DB::table('chitietsanpham')->where('MaSP', (string)$id)->where('Size', (string)$size)->get('ChiTietSPID')[0]->ChiTietSPID;
+        // $data = (DB::table('chitietsanpham')->where('MaSP', (string)$id)->where('Size', (string)$size)->get('ChiTietSPID'));
+        return $data;
+    }
+    public function getCT($id){
+
+    }
+}
