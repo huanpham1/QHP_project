0db3ec39d6653060b97e554bbabfc226a3658870
Le Hong Phong <phong2552001@gmail.com>
2022-06-05 15:21:46 +0700

    nothing

--- QHP_project/app/Http/Controllers/homeController.php
+++ QHP_project/app/Http/Controllers/homeController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Session\Session;
+use App\Models\LaySanPham;
+use App\Models\LayTheLoai;
+use App\Models\LayDanhMuc;
 
 class homeController extends Controller
 {
